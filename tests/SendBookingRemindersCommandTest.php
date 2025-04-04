6f1e6942a4bc2dd4a3ff9099f13de001bb2f87fc
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2025-04-04 18:43:51 +0200

    - SendBookingRemindersCommandTest - testNoRemindersSent() - testRemindersSent()
    (Outside-In Testing)

---
+++ tests/SendBookingRemindersCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Tests;
+
+use App\Factory\BookingFactory;
+use App\Factory\CustomerFactory;
+use App\Factory\TripFactory;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Console\Test\InteractsWithConsole;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+use Zenstruck\Mailer\Test\InteractsWithMailer;
+use Zenstruck\Mailer\Test\TestEmail;
+
+class SendBookingRemindersCommandTest extends KernelTestCase
+{
+    use ResetDatabase, Factories, InteractsWithMailer, InteractsWithConsole;
+
+    public function testNoRemindersSent()
+    {
+        $this->executeConsoleCommand('app:send-booking-reminders')
+            ->assertSuccessful()
+            ->assertOutputContains('Sent 0 booking reminders');
+    }
+
+    public function testRemindersSent()
+    {
+        $booking = BookingFactory::createOne([
+            'trip' => TripFactory::new([
+                'name' => 'Visit Mars',
+                'slug' => 'iss',
+            ]),
+            'customer' => CustomerFactory::new([
+                'email' => 'john@doe.com',
+            ]),
+            'date' => new \DateTimeImmutable('+4 days'),
+        ]);
+
+        $this->assertNull($booking->getReminderSentAt());
+
+        $this->executeConsoleCommand('app:send-booking-reminders')
+            ->assertSuccessful()
+            ->assertOutputContains('Sent 1 booking reminders');
+
+        $this->mailer()
+             ->assertSentEmailCount(1)
+             ->assertEmailSentTo('john@doe.com', function (TestEmail $email) {
+                 $email
+                     ->assertsubject('Booking confirmation for Visit Mars')
+                     ->assertContains('Visit Mars')
+                     ->assertContains('/booking/'.BookingFactory::first()->getUid());
+             });
+
+        $this->assertNotNull($booking->getReminderSentAt());
+    }
+}
